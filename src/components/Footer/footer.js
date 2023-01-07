65920c8da6d36e790cba6e37ec82b7c61cb749fb
1abdllyva <ayselabdullayeva657@gmail.com>
2023-01-07 05:49:57 -0800

    add

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,18 @@
+import "./footer.css"
+function Footer() {
+    return ( 
+        <div className="footerDiv">
+            <div className="footerCont">
+                 
+                <div className="contactUsContainer">
+                    <h3 className="contactUs">Əlaqə saxlayın</h3>
+                    <a href="mailto: tunzaleimanova03@mail.ru" className="email">abdllyvaysl03@gmail.com</a>
+                    <a href="tel: +994-055-55-55" className="email">+994-070-526-39-52</a>
+                </div>
+                <div className="copyright">© 2022 SunWeather. Bütün hüquqlar qorunur.</div>
+            </div>
+        </div> 
+    );
+}
+
+export default Footer;
---
+++ src/state/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import WeatherApiReducers from "./weatherApiReducers";
+const rootReducer = combineReducers({
+    search: WeatherApiReducers
+})
+
+export default rootReducer;
